c78e4eade57cf62a3edc4649d2971229b73b688c
yash270998 <yashsomani2007@gmail.com>
2021-03-08 15:57:17 +0000

    Final Commit

---
+++ Controllers/WordController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class WordController : Controller
+    {
+        // GET: Word
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult postWord(string word)
+        {
+            //....
+            Word newWord = new Word();
+            newWord.word = word;
+            //            return "<b>ok</b>" + word;
+            ViewBag.Message = newWord;
+
+            return View();
+
+
+        }
+
+    }
+}
